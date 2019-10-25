c0cb0ac809db9d3feda6ff317d27a225770757a3
Jefferson Lima <jeffersonlima94@hotmail.com>
2019-10-25 17:25:10 -0300

    Agendamento de serviço

---
+++ modulo03/src/app/controllers/AppointmentController.js
@@ -0,0 +1,37 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { provider_id, date } = req.body;
+    /*
+    Verificar se o provider_id é um provider
+    */
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appoitments with providers' });
+    }
+    /* Criando agendamento */
+
+    const appointment = await Appointment.create({
+      user_id: req.user_Id,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment;
+  }
+}
+export default new AppointmentController();
